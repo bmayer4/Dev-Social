966ba5e8e46ac72fdf134ffbb10fa7c9290d6bd2
bmayer4 <bmayer55@yahoo.com>
2017-07-27 21:31:36 -0400

    first commit

---
+++ Dev Social/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  SignInVC.swift
+//  Dev Social
+//
+//  Created by Brett Mayer on 7/27/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
