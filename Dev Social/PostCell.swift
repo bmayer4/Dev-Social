bf8bbff538c181d4cf475175d95d81d7e2ba22be
bmayer4 <bmayer55@yahoo.com>
2017-08-03 20:57:51 -0400

    made changes

---
+++ Dev Social/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Dev Social
+//
+//  Created by Brett Mayer on 7/31/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
