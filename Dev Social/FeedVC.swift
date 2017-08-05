fced37ffba39ec26e68f3aa70083b865f07e9fa2
bmayer4 <bmayer55@yahoo.com>
2017-08-05 17:50:09 -0400

    firebase get completed

--- Dev Social/FeedVC.swift
+++ Dev Social/FeedVC.swift
@@ -13,6 +13,8 @@ import Firebase
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +28,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
                 for snap in snapshot {
                     print("SNAP: \(snap)")
+                    
+                    if let postDic = snap.value as? Dictionary<String, Any> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDic)
+                        self.posts.append(post)
+                    }
                 }
             }
-            
+            self.tableView.reloadData()
         })
     }
     
@@ -37,10 +45,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post  = posts[indexPath.row]
+        print("CAP: \(post.caption)")
+
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell //temp
     }
     
